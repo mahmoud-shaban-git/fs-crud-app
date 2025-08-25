090b23d2cc8e8401eadb07eec373d7fb37305b99
mahmoud <mahmoud.shaban@stud-mail.uni-wuerzburg.de>
2025-08-25 11:09:26 +0200

    Add global .gitignore and clean repo

--- crud-app/src/main/java/com/springprogramming/crud_app/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.springprogramming.crud_app.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Long id) {
-        super("Der Benutzer mir dieser ID: " + id + "Konnten nicht gefunden werden!");
-    }
-}
--- crud-app/src/main/java/com/springprogramming/crud_app/repository/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.springprogramming.crud_app.repository;
-
-import com.springprogramming.crud_app.model.Benutzer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<Benutzer, Long> {
-}
